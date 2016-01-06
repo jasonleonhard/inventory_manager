5cfd6f4a25d0f4177fc0da28b9d06e14d1823d6e
un5t0ppab13 <devbrights@gmail.com>
2016-01-06 13:18:39 -0800

    TestMenu#test_show_JSON_responds, test_filename_responds, test_questions_responds, describe('Menu#setup') do

--- tests/TestMenu.rb
+++ tests/TestMenu.rb
@@ -16,11 +16,28 @@ class TestMenu < MiniTest::Test
     assert_respond_to @menu, :show_JSON
   end
 
+  def test_filename_responds
+    assert_respond_to @menu, :filename
+  end
+
+  def test_questions_responds
+    assert_respond_to @menu, :questions
+  end
+
   def call
     @menu.filename
+    @menu.questions
   end
 
   def test_call_responds
     self.call
   end
+
+  describe('Menu#setup') do
+    it('is nil') do
+      @menu.must_equal nil
+    end
+  end
+
+
 end
