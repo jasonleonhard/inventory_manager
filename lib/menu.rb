eb79d964191683e67786ceb504259d525c9b3993
un5t0ppab13 <devbrights@gmail.com>
2016-01-06 12:52:08 -0800

    menu#f show_JSON

--- lib/menu.rb
+++ lib/menu.rb
@@ -20,8 +20,14 @@ class Menu
   end
 
   def questions
-    p 'Choose one: a, b, c, d, or e'
-      input = gets.chomp
+    puts 'Choose one: a, b, c, d, or e'
+    puts 'a. What are the 5 most expensive items from each category?'
+    puts 'b. Which cds have a total running time longer than 60 minutes?'
+    puts 'c. Which authors have also released cds?'
+    puts 'd. Which items have a title, track, or chapter that contains a year?'
+    puts 'e. exit, quit, e, q, x'
+    puts 'f. Show JSON file'
+    input = gets.chomp
     if input == 'a'
       self.a
     elsif input == 'b'
@@ -30,9 +36,11 @@ class Menu
       self.c
     elsif input == 'd'
       self.d
-    elsif %w(x exit quit q e).member?(input)
+    elsif %w(exit quit e q x).member?(input)
       puts "Thank you, exiting now."
       self.exit
+    elsif input == 'f'
+      self.f
     else
       p "#{input} is not an option, please chose again: "
       self.questions
@@ -40,21 +48,30 @@ class Menu
   end
 
   def a
+    puts 'a. What are the 5 most expensive items from each category?'
     self.questions
   end
 
   def b
+    puts 'b. Which cds have a total running time longer than 60 minutes?'
     self.questions
   end
 
   def c
+    puts 'c. Which authors have also released cds?'
     self.questions
   end
 
   def d
+    puts 'd. Which items have a title, track, or chapter that contains a year?'
     self.questions
   end
 
   def exit
+  end
+
+  def f
+    self.show_JSON
+    self.questions
   end
 end
